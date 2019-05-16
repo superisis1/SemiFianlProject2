b18b562c88ab60bd8047ab667372b481a767a7a3
Insung Jung <superisis@gmail.com>
2019-05-16 18:12:00 +0900

    로그인 구현  - LoginActivity.java

--- app/src/main/java/kr/insungjung/semifianlproject2/LoginActivity.java
+++ app/src/main/java/kr/insungjung/semifianlproject2/LoginActivity.java
@@ -1,13 +1,84 @@
 package kr.insungjung.semifianlproject2;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import kr.insungjung.semifianlproject2.databinding.ActivityLoginBinding;
+import kr.insungjung.semifianlproject2.utils.ConnectServer;
+
+public class LoginActivity extends BaseActivity {
+
+    ActivityLoginBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+        act.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inputId = act.loginIdEdt.getText().toString();
+                String inputPw = act.loginPwEdt.getText().toString();
+
+                ConnectServer.postRequestLogIn(mContext, inputId, inputPw, new ConnectServer.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    int code = json.getInt("code");
+                                    Log.d("로그인 코드", Integer.toString(code));
+
+                                    if(code == 200) {
+                                        Intent intent = new Intent(mContext, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    } else {
+                                        AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                                        alert.setTitle("로그인 실패");
+                                        alert.setMessage(json.getString("message"));
+                                        alert.setPositiveButton("확인", null);
+                                        alert.show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this, R.layout.activity_login);
     }
 }
